8ce9a337822595e56e543c8602bc6749459290d7
harsha06anblicks@gmail.com <harsha06anblicks@gmail.com>
2023-06-26 06:12:11 +0000

    Extracted directly from raw schema instead of source

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,4 +1,4 @@
-/* with customers as (
+with customers as (
     
     select 
         id as customer_id,
@@ -9,7 +9,7 @@
 )
 
 select * from customers
-*/
+
 /*
 with source as (
     slect * fro raw.jaffle_shop.customers
@@ -23,9 +23,10 @@ staged as (
 )
 
 select * from staged
-*/
+
 select 
     id as customer_id,
     first_name,
     last_name
 from {{ source('jaffle_shop', 'customers') }}
+*/
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,4 +1,4 @@
-/*with orders as (
+with orders as (
     
     select
         id as order_id,
@@ -10,10 +10,11 @@
 )
 
 select * from orders
-*/
+/*
 select
     id as order_id,
     user_id as customer_id,
     order_date,
     status
 from {{ source('jaffle_shop', 'orders') }}
+*/
